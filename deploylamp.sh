b7af40a03323219891a3f43614cec8bdc643382d
Ken Studdy <kenstuddy@users.noreply.github.com>
2018-03-30 10:31:13 -0400

    Added PHP 5.6

--- deploylamp.sh
+++ deploylamp.sh
@@ -15,8 +15,8 @@ curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get update && sudo apt-get dist-upgrade -y
 sudo ufw allow 80
 sudo ufw allow 443
-sudo apt-get install nodejs p7zip-full openssh-server apache2 apache2-utils mysql-server php7.2 php7.2-curl php7.2-cgi libapache2-mod-php php-mysql php7.2-dom php7.2-mbstring unzip -y
-sudo apt-get install php7.2-xml php7.2-zip libxslt1.1 -y
+sudo apt-get install nodejs p7zip-full openssh-server apache2 apache2-utils mysql-server php7.2 php7.2-curl php7.2-cgi libapache2-mod-php7.2 php-mysql php7.2-dom php7.2-mbstring unzip php7.2-xml php7.2-zip libxslt1.1 -y
+sudo apt-get install libapache2-mod-php5.6 php5.6 php5.6-curl php5.6-cgi php5.6-dom php5.6-mbstring php5.6-xml php5.6-zip -y
 sudo mysql_secure_installation
 #Done with installing LAMP, now it is time to secure the server
 sudo apt-get install fail2ban psad rkhunter chkrootkit -y
@@ -38,12 +38,21 @@ fi
 sudo find /var/www/html \( -type f -execdir chmod 644 {} \; \) \
                   -o \( -type d -execdir chmod 711 {} \; \)
 sudo chown -R www-data:www-data /var/www/html
+sudo ufw enable
 sudo a2enmod headers
 sudo service apache2 restart
-#sudo chkrootkit
-#sudo rkhunter --update
-#sudo rkhunter --propupd
-#sudo rkhunter --check
-#sudo apt-get install wapiti -y
-#wapiti http://example.org -n 10 -b folder
-sudo ufw enable
+
+#To enable php 7.2
+sudo a2dismod php5.6
+sudo a2enmod php7.2
+sudo service apache2 restart
+sudo update-alternatives --set php /usr/bin/php7.2
+sudo update-alternatives --set phar /usr/bin/phar7.2
+sudo update-alternatives --set phar.phar /usr/bin/phar.phar7.2
+#To enable php 5.6
+#sudo a2dismod php7.2
+#sudo a2enmod php5.6
+#sudo service apache2 restart
+#sudo update-alternatives --set php /usr/bin/php5.6
+#sudo update-alternatives --set phar /usr/bin/phar5.6
+#sudo update-alternatives --set phar.phar /usr/bin/phar.phar5.6 
